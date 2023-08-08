d55eef01512df856d77d6fb0652d0f0c8426b00c
Ruth Westnidge <rwestnidge@gmail.com>
2023-08-08 16:26:00 +0100

    WIP barebones client-side validation Login. Needs granular control over server-side error messages

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,7 @@
 /* TODO:
 temporary log in, replace with SSO & avoid user details in local storage */
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLogin } from "../hooks/useLogin";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -10,10 +10,30 @@ import { faRightToBracket, faUserPlus } from "@fortawesome/free-solid-svg-icons"
 const Login = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    // form has server-side & client-side validation
     const { login, error, isLoading } = useLogin()
+
+    // client-side validation prevent empty fields 
+    const [isFormValid, setIsFormValid] = useState(false)
+    const [trySubmit, setTrySubmit] = useState(false)
+
+    useEffect(() => {
+        console.log("Email", email)
+        console.log("Password", password)
+        if (email && password) {
+            setIsFormValid(true)
+        } else {
+            setIsFormValid(false)
+        }
+    }, [email, password])
 
     const handleSubmit = async (e) => {
         e.preventDefault()
+
+        if (!isFormValid) {
+            setTrySubmit(true)
+            return
+        }
 
         await login(email, password)
     }
@@ -24,26 +44,48 @@ const Login = () => {
         navigate("/Signup")
     }
 
+    // client-side - sets frontend error display according to missing fields
+    const missingFields = () => {
+        let fields = []
+        if (!email) fields.push("Email")
+        if (!password) fields.push("Password")
+        return fields
+    }
+
     const loginIcon = <FontAwesomeIcon icon={faRightToBracket} />
     const signupIcon = <FontAwesomeIcon icon={faUserPlus} />
- 
+
     return (
         <>
             <form className="login" onSubmit={handleSubmit}>
+                {(trySubmit && !isFormValid) || error ? (
+                    <div className="error">
+                        {trySubmit && !isFormValid ? (
+                            <>
+                                Please fill in the following fields: {missingFields().join(", ")}
+                            </>
+                        ) : (
+                            error
+                        )}
+                    </div>
+                ) : null}
+
                 <h1>Welcome. Login here:</h1>
                 <label>Email*</label>
                 <input
-                   
+                    id="email"
                     type="email"
                     onChange={(e) => setEmail(e.target.value)}
                     value={email}
+                    className={(trySubmit && !email) || missingFields().includes("email") ? "error" : "primary"}
                 />
                 <label>Password*</label>
                 <input
-                    
+                    id="password"
                     type="password"
                     onChange={(e) => setPassword(e.target.value)}
                     value={password}
+                    className={(trySubmit && !password) || missingFields().includes("password") ? "error" : "primary"}
                 />
 
                 <div >
