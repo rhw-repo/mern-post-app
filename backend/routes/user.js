fcfa3d9b456a3881ccf984d67ac98838d5bed4ac
Ruth Westnidge <rwestnidge@gmail.com>
2023-08-15 20:51:27 +0100

    WIP adds empty field and not password validation to login using express-validator

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,4 +1,6 @@
 const express = require("express")
+const { body, validationResult } = require("express-validator")
+
 
 // controller functions
 const { loginUser,  signupUser } = require("../controllers/userController")
@@ -6,10 +8,27 @@ const { loginUser,  signupUser } = require("../controllers/userController")
 // invokes to make an instance of the express router
 const router = express.Router()
 
-//log in route
-router.post("/login", loginUser)
+//log in route 
+// #1 express-validator confimed with Postman
+
+router.post('/login', [
+    body('email').not().isEmpty().withMessage('Email is required').bail()
+               .isEmail().withMessage('Invalid email format'),
+    body('password').not().isEmpty().withMessage('Password is required'),
+  ], (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  }, loginUser);
+
+// #2 validator confimed with Postman
+//router.post("/login", loginUser)
 
 //signup route
 router.post("/signup", signupUser)
+
+
 
 module.exports = router
