23987136a5ac149d5a4d541dba04685d08a1c59b
Ruth Westnidge <rwestnidge@gmail.com>
2023-09-01 15:47:41 +0100

    WIP adds further validation

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -9,7 +9,8 @@ const { loginUser,  signupUser } = require("../controllers/userController")
 const router = express.Router()
 
 //log in route 
-/* #1 express-validator confimed with Postman
+// #1 express-validator confimed with Postman
+// checks for empty fields, invalid email format
 
 router.post('/login', [
     body('email').not().isEmpty().withMessage('Email is required').bail()
@@ -21,12 +22,27 @@ router.post('/login', [
       return res.status(422).json({ errors: errors.array() });
     }
     next();
-  }, loginUser);*/
+  }, loginUser);
 
 // #2 validator confimed with Postman
-router.post("/login", loginUser)
+//router.post("/login", loginUser)
 
 //signup route
+// #1 express-validator 
+// checks for empty fields, invalid email format
+router.post("/signup", [
+    body('email').not().isEmpty().withMessage('Email is required').bail()
+               .isEmail().withMessage('Invalid email format'),
+    body('password').not().isEmpty().withMessage('Password is required'),
+  ], (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+    next();
+  }, signupUser);
+
+// #2 validator
 router.post("/signup", signupUser)
 
 
