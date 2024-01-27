d59800161273f74f33ca8da6ca2ffdf83f32cbe0
Ruth Westnidge <rwestnidge@gmail.com>
2024-01-27 17:12:28 +0000

    ContentDetail: adds Title and Content ensures logical order screen readers & understandability generally

--- frontend/src/pages/ContentDetail/ContentDetail.js
+++ frontend/src/pages/ContentDetail/ContentDetail.js
@@ -27,40 +27,44 @@ const ContentDetail = () => {
 
     return (
         <>
-        <div className={styles.contentDetailWrapper}>
-            <div className="content-detail-edit-create-containers">
-                {isEditing ? (
-                    <Edit material={material} onUpdateComplete={handleUpdateComplete} />
-                ) : (
-                    <div>
-                        {/*  <h3>Id is {_id}</h3> */}
-                        {isPending && <div>Loading...</div>}
-                        {error && <div>{error}</div>}
-                        {material && (
-                            <article>
-                                <div className={styles.contentDetailTitle}>{material.title}</div>
-                                <div className={styles.contentDetailContent}>{material.content}</div>
-                                <div className={`${styles.contentDetailTagsLabel} document-form-headings`}>Tags:</div>
-                                <div className="content-detail-tags-container">
-                                    {material.tags.map((tag, index) => (
-                                        <span key={index} className={` ${styles.tags} ${styles.tagChip} tags tag-chip`}>{tag}</span>
-                                    ))}
-                                </div>
-                            </article>
+            <main>
+                <div className={styles.contentDetailWrapper}>
+                    <div className="content-detail-edit-create-containers">
+                        {isEditing ? (
+                            <Edit material={material} onUpdateComplete={handleUpdateComplete} />
+                        ) : (
+                            <div>
+                                {/*  <h3>Id is {_id}</h3> */}
+                                {isPending && <div>Loading...</div>}
+                                {error && <div>{error}</div>}
+                                {material && (
+                                    <article>
+                                        <div className={`${styles.contentDetailTitleLabel} document-form-headings`}>Title</div>
+                                        <div className={styles.contentDetailTitle}>{material.title}</div>
+                                        <div className={`${styles.contentDetailTitleLabel} document-form-headings`}>Content</div>
+                                        <div className={styles.contentDetailContent}>{material.content}</div>
+                                        <div className={`${styles.contentDetailTagsLabel} document-form-headings`}>Tags:</div>
+                                        <div className="content-detail-tags-container" aria-label="List of tags">
+                                            {material.tags.map((tag, index) => (
+                                                <span key={index} className={` ${styles.tags} ${styles.tagChip} tags tag-chip`}>{tag}</span>
+                                            ))}
+                                        </div>
+                                    </article>
+                                )}
+                                <section className="content-detail-edit-create-btns">
+                                    <CancelButton />
+                                    <button
+                                        className={`go-to-edit-btn ${styles.goToEditBtn}`}
+                                        onClick={handleUpdateClick}>
+                                        {editIcon} Edit
+                                    </button>
+                                </section>
+                            </div>
                         )}
-                        <div className="content-detail-edit-create-btns">
-                            <CancelButton />
-                            <button
-                                className={`go-to-edit-btn ${styles.goToEditBtn}`}
-                                onClick={handleUpdateClick}>
-                                {editIcon} Edit
-                            </button>
-                        </div>
-                    </div>
-                )}
 
-            </div>
-            </div>
+                    </div>
+                </div>
+            </main>
         </>
     )
 }
